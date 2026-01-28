c10549d353bbada7bbc8aea01de09f628c00e77b
ryanocerrous <antiheroryan@gmail.com>
2026-01-28 15:46:29 +0000

    Wait for X display before launching; avoid race on boot

--- run_app.sh
+++ run_app.sh
@@ -4,15 +4,24 @@ set -e
 APP="/home/kali/ProductionProjectUI/src/app.py"
 PY="/usr/bin/python3"
 
-# Use an existing X server; if none, exit quietly (service will be restarted later).
-if [ -S /tmp/.X11-unix/X0 ]; then
-  export DISPLAY=":0"
-elif [ -S /tmp/.X11-unix/X1 ]; then
-  export DISPLAY=":1"
-else
+# Wait for an existing X server to appear (up to 25s)
+for i in $(seq 1 50); do
+  if [ -S /tmp/.X11-unix/X0 ]; then
+    DISPLAY_TARGET=":0"
+    break
+  elif [ -S /tmp/.X11-unix/X1 ]; then
+    DISPLAY_TARGET=":1"
+    break
+  fi
+  sleep 0.5
+done
+
+# If still no display, bail quietly; systemd will restart and retry
+if [ -z "${DISPLAY_TARGET:-}" ]; then
   exit 0
 fi
 
+export DISPLAY="$DISPLAY_TARGET"
 export XAUTHORITY="/home/kali/.Xauthority"
 
 # Avoid multiple instances
